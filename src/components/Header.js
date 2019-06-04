7846cdaf02cab35a00a5a8eba30cafe39740525c
KpoJIuk06 <ramizomarov06@gmail.com>
2019-06-04 18:43:31 +0400

    testing

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import  React from 'react';
+
+
+const Header =() =>{
+ return(
+    <header className='header'>
+        <img src='https://logos-download.com/wp-content/uploads/2019/01/Cosmos_Logo-700x700.png' alt=""/>
+    </header>
+ )
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile =()=>{
+   return <div className='content'>
+        <div>
+            <img src="http://theperfectlight.com/wp-content/uploads/2017/01/Light-Pollution.png" alt=""/>
+        </div>
+        <div>
+            ava+description
+        </div>
+        <div>
+            My posts
+
+            <div>
+                New Post
+            </div>
+            <div>
+                Post 1
+            </div>
+            <div>
+                Post 2
+            </div>
+        </div>
+    </div>
+};
+
+
+
+export default Profile;
